5dda9cb3cc0f9f2aded066d32109389bcecc0347
t7037453 <t7037453@TU39276>
2019-11-14 14:45:54 +0000

    Added DTO's

---
+++ ManagementApp.Data/PurchaseRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagementApp.Data
+{
+    class PurchaseRequest
+    {
+        public int PurchaseRequestID { get; set; }
+        public int StaffID { get; set; }
+        public int VendorID { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set;}
+        public int ProductQty { get; set; }
+        public int PaymentDetailsID { get; set; }
+        // Maybe have ability to add card details, then select them from drop down when approving.
+        public PaymentDetails Payment { get; set: }
+    }
+}
---
+++ ManagementApp.Data/StaffAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagementApp.Data
+{
+    class StaffAccount
+    {
+        public int StaffID { get; set; }
+        public string Name { get; set; }
+        public int PermissionID { get; set; }
+        // Permissions Table with all Auth levels and actions (depends how auth server works)
+        public Permission Permissions { get; set; }
+    }
+}
