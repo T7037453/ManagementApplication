42599fbe6614a2aab40295bb90a8011f7fda5d13
Jordan Salmons <43573188+T7037453@users.noreply.github.com>
2020-01-07 13:44:12 +0000

    can see perms and staff accounts

--- Management_Application/Models/PaymentDetailsDto.cs
+++ Management_Application/Models/PaymentDetailsDto.cs
@@ -7,6 +7,10 @@ namespace ManagementApp.Web.Models
 {
     public class PaymentDetailsDto
     {
+        public int Id { get; set; }
+        public int PaymentDetailsID { get; set; }
+        public string AccountNumber { get; set; }
+        public string SortCode { get; set; }
 
     }
 }
---
+++ Management_Application/Models/PermissionsDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementApp.Web.Models
+{
+    public class PermissionsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool ViewStock { get; set; }
+        public bool CreatePurchaseRequest { get; set; }
+        public bool ReSell { get; set; }
+        public bool PendingInvoices { get; set; }
+        public bool ViewOrders { get; set; }
+        public bool PurchaseControl { get; set; }
+        public bool ViewProfile { get; set; }
+        public bool HideReview { get; set; }
+        public bool DeleteCustomer { get; set; }
+    }
+}
